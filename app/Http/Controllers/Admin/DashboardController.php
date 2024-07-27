1ff71fe18328fa1050108dd2c7c39b0fa03ef124
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-07-27 18:39:38 +0600

    dashboard summery added

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Modules\Customer\app\Models\CustomerDue;
 use Modules\Language\app\Models\Language;
+use Modules\Product\app\Models\Product;
+use Modules\Sales\app\Models\Sale;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $data['customerDues'] = CustomerDue::where('status', 1)->sum('due_amount');
+        $data['todaySales'] = Sale::whereDate('order_date', date('Y-m-d'))->sum('grand_total');
+        $data['totalProducts'] = Product::count();
+        $data['suppliersDues'] = 0;
+        return view('admin.dashboard', compact('data'));
     }
 
     public function setLanguage()
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,13 +4,14 @@
 @endsection
 @section('admin-content')
     <div class="main-content">
-        
+
         @if ($setting->is_queable == 'active' && Cache::get('corn_working') !== 'working')
             <div class="alert alert-danger alert-has-icon alert-dismissible show fade">
                 <div class="alert-icon"><i class="fas fa-sync"></i></div>
                 <div class="alert-body">
                     <div class="alert-title"><a href="{{ route('admin.general-setting') }}" target="_blank"
-                            rel="noopener noreferrer">{{ __('Corn Job Is Not Running! Many features will be disabled and face errors') }}</a></div>
+                            rel="noopener noreferrer">{{ __('Corn Job Is Not Running! Many features will be disabled and face errors') }}</a>
+                    </div>
                     <button class="close" data-dismiss="alert">
                         <span><i class="fas fa-times"></i></span>
                     </button>
@@ -25,9 +26,69 @@
 
             <div class="section-body">
                 <div class="row">
-                    <div class="col-12">
-
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-primary">
+                                <i class="fas fa-money-bill-alt"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>{{ __('Customer Due') }}</h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ currency($data['customerDues']) }}
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-info">
+                                <i class="fas fa-money-bill-alt"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>{{ __('Supplier Due') }}</h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ currency($data['suppliersDues']) }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-info">
+                                <i class="fas fa-shopping-bag"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>{{ __('Total Products') }}</h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ $data['totalProducts'] }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-info">
+                                <i class="fas fa-money-bill-alt"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>{{ __('Today Sales') }}</h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ currency($data['todaySales']) }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </section>
