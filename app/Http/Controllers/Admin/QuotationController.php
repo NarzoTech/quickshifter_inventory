1086dfa8920cea31692b6c480c299b3cca7db72e
Rizvi Ahmed <prog.rizvi@gmail.com>
2024-08-20 00:12:16 +0600

    quotation stored

--- app/Http/Controllers/Admin/QuotationController.php
+++ app/Http/Controllers/Admin/QuotationController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\QuotationRequest;
+use App\Models\Quotation;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Modules\Product\app\Models\Product;
 
 class QuotationController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
     /**
      * Display a listing of the resource.
      */
@@ -35,11 +42,50 @@ class QuotationController extends Controller
      */
     public function store(QuotationRequest $request)
     {
+        DB::beginTransaction();
 
         try {
             // create quotation
+
+            $quotation = Quotation::create([
+                'customer_id' => $request->customer_id,
+                'date' => now()->parse($request->date),
+                'note' => $request->note,
+                'subtotal' => $request->subtotal,
+                'discount' => $request->discount,
+                'after_discount' => $request->after_discount,
+                'vat' => $request->vat,
+                'total' => $request->total_amount,
+                'created_by' => auth('admin')->user()->id,
+                // 'warehouse_id' => $request->warehouse_id,
+            ]);
+
+
+            // create quotation details
+            foreach ($request->product_id as $key => $product_id) {
+
+                $quotation->details()->create([
+                    'product_id' => $product_id,
+                    'quantity' => $request->quantity[$key],
+                    'price' => $request->unit_price[$key],
+                    'sub_total' => $request->total[$key],
+                ]);
+            }
+
+
+            DB::commit();
+            return redirect()->route('admin.quotation.index')->with([
+                'alert-type' => 'success',
+                'messege' => 'Quotation created successfully'
+            ]);
         } catch (\Exception $ex) {
-            //throw $th;
+
+            DB::rollBack();
+            Log::error($ex->getMessage());
+            return redirect()->back()->with([
+                'alert-type' => 'error',
+                'messege' => $ex->getMessage()
+            ]);
         }
     }
 
