823c1a239571ecbcc35c62f01acd915b99ce2611
progRizvi <prog.rizvi@gmail.com>
2024-11-24 20:05:31 +0600

    purchase sidebar issue fix

--- Modules/Product/resources/views/sidebar.blade.php
+++ Modules/Product/resources/views/sidebar.blade.php
@@ -1,111 +1,54 @@
-{{-- <li class="menu-header">{{ __('Manage Product') }}</li>
+<li class="app-sidebar__heading">{{ __('Manage Product') }}</li>
 
 <li
-    class="nav-item dropdown {{ Route::is('admin.product.*') || Route::is('admin.unit.*') || Route::is('admin.category.*') || Route::is('admin.brand.*') ? 'active' : '' }}">
-    <a href="javascript:void()" class="nav-link has-dropdown"><i
-            class="fas fa-newspaper"></i><span>{{ __('Manage Products') }}</span></a>
-
-    <ul class="dropdown-menu">
-        <li
-            class="{{ Route::is('admin.product.index') || Route::is('admin.product.edit') || Route::is('admin.product.show') ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('admin.product.index') }}">
-                {{ __('Product List') }}
-            </a>
-        </li>
-        <li class="{{ Route::is('admin.product.create') ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('admin.product.create') }}">
-                {{ __('Add Product') }}
-            </a>
-        </li>
-        <li class="{{ Route::is('admin.unit*') ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('admin.unit.index') }}">
-                {{ __('Unit Type') }}
-            </a>
-        </li>
-        <li class="{{ Route::is('admin.category*') ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('admin.category.index') }}">
-                {{ __('Category') }}
-            </a>
-        </li>
-        <li class="{{ Route::is('admin.brand*') ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('admin.brand.index') }}">
-                {{ __('Brand') }}
-            </a>
-        </li>
-        <li class="{{ Route::is('admin.attribute*') ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('admin.attribute.index') }}">
-                {{ __('Attribute') }}
-            </a>
-        </li>
-
-
-        <li class="{{ Route::is('admin.product.barcode') ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('admin.product.barcode') }}">
-                {{ __('Print Barcode') }} / {{ __('Label') }}
-            </a>
-        </li>
-    </ul>
-</li>
-
- --}}
-
-
-
-
-<li>
+    class="{{ Route::is('admin.product.*') || Route::is('admin.unit.*') || Route::is('admin.category.*') || Route::is('admin.brand.*') ? 'mm-active' : '' }}">
     <a href="javascript:;">
+
         <i class="metismenu-icon pe-7s-plugin"></i> {{ __('Manage Product') }}
         <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
     </a>
-    <ul>
+
+    <ul
+        class="mm-collapse {{ Route::is('admin.product.*') || Route::is('admin.unit.*') || Route::is('admin.category.*') || Route::is('admin.brand.*') ? 'mm-show' : '' }}">
         <li>
-            <a href="{{ route('admin.product.index') }}">
-                <i class="metismenu-icon">
-                </i>
+            <a href="{{ route('admin.product.index') }}"
+                class="{{ Route::is('admin.product.index') || Route::is('admin.product.edit') || Route::is('admin.product.show') ? 'mm-active' : '' }}">
                 {{ __('Product List') }}
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.product.create') }}">
-                <i class="metismenu-icon">
-                </i>
+            <a href="{{ route('admin.product.create') }}"
+                class="{{ Route::is('admin.product.create') ? 'mm-active' : '' }}">
                 {{ __('Add Product') }}
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.unit.index') }}">
-                <i class="metismenu-icon">
-                </i>
+            <a href="{{ route('admin.unit.index') }}" class="{{ Route::is('admin.unit*') ? 'mm-active' : '' }}">
                 {{ __('Unit Type') }}
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.category.index') }}">
-                <i class="metismenu-icon">
-                </i>
+            <a href="{{ route('admin.category.index') }}"
+                class="{{ Route::is('admin.category*') ? 'mm-active' : '' }}">
                 {{ __('Category') }}
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.brand.index') }}">
-                <i class="metismenu-icon">
-                </i>
+            <a href="{{ route('admin.brand.index') }}" class="{{ Route::is('admin.brand*') ? 'mm-active' : '' }}">
                 {{ __('Brand') }}
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.attribute.index') }}">
-                <i class="metismenu-icon">
-                </i>
+            <a href="{{ route('admin.attribute.index') }}"
+                class="{{ Route::is('admin.attribute*') ? 'mm-active' : '' }}">
                 {{ __('Attribute') }}
             </a>
         </li>
 
 
         <li>
-            <a href="{{ route('admin.product.barcode') }}">
-                <i class="metismenu-icon">
-                </i>
+            <a href="{{ route('admin.product.barcode') }}"
+                class="{{ Route::is('admin.product.barcode') ? 'mm-active' : '' }}">
                 {{ __('Print Barcode') }} / {{ __('Label') }}
             </a>
         </li>
--- Modules/Purchase/resources/views/sidebar.blade.php
+++ Modules/Purchase/resources/views/sidebar.blade.php
@@ -1,9 +1,10 @@
-{{-- <li>
-    <a href="javascript:void()" class="nav-link has-dropdown">
-        <i class="fas fa-cart-arrow-down"></i><span>{{ __('Manage Purchase') }}</span>
+<li class="{{ Route::is('admin.purchase.*') ? 'mm-active' : '' }}">
+    <a href="javascript:;">
+        <i class="metismenu-icon pe-7s-plugin"></i> {{ __('Manage Purchase') }}
+        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
     </a>
 
-    <ul class="dropdown-menu">
+    <ul class="mm-collapse {{ Route::is('admin.purchase.*') ? 'mm-show' : '' }}">
         <li>
             <a class="nav-link" href="{{ route('admin.purchase.create') }}">
                 {{ __('Add Purchase') }}
@@ -21,46 +22,6 @@
         </li>
         <li class="{{ isRoute('admin.purchase.return.type.list') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('admin.purchase.return.type.list') }}">
-                {{ __('Purchases Return Type') }}
-            </a>
-        </li>
-    </ul>
-</li> --}}
-
-
-
-
-<li>
-    <a href="javascript:;">
-        <i class="metismenu-icon pe-7s-plugin"></i> {{ __('Manage Purchase') }}
-        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
-    </a>
-    <ul>
-        <li>
-            <a href="{{ route('admin.purchase.create') }}">
-                <i class="metismenu-icon">
-                </i>
-                {{ __('Add Purchase') }}
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('admin.purchase.index') }}">
-                <i class="metismenu-icon">
-                </i>
-                {{ __('Manage Purchase') }}
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('admin.purchase.return.index') }}">
-                <i class="metismenu-icon">
-                </i>
-                {{ __('Purchases Return List') }}
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('admin.purchase.return.type.list') }}">
-                <i class="metismenu-icon">
-                </i>
                 {{ __('Purchases Return Type') }}
             </a>
         </li>
