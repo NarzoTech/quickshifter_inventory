663c26992b7f129de4524c9c2d5c8dbc5362a89a
progRizvi <prog.rizvi@gmail.com>
2024-11-30 13:57:12 +0600

    supplier search issue fix

--- main_files/Modules/Supplier/app/Services/SupplierService.php
+++ main_files/Modules/Supplier/app/Services/SupplierService.php
@@ -29,10 +29,10 @@ class SupplierService
             if (request()->from_date || request()->to_date) {
                 [$from_date, $to_date] = $this->getDateRangeFromRequest();
                 if ($from_date) {
-                    $query->where('date', '>=', $from_date);
+                    $query->where('purchase_date', '>=', $from_date);
                 }
                 if ($to_date) {
-                    $query->where('date', '<=', $to_date);
+                    $query->where('purchase_date', '<=', $to_date);
                 }
             }
         }, 'payments' => function ($query) {
@@ -41,11 +41,11 @@ class SupplierService
             [$from_date, $to_date] = $this->getDateRangeFromRequest();
 
             if ($from_date) {
-                $query->where('date', '>=', $from_date);
+                $query->where('payment_date', '>=', $from_date);
             }
 
             if ($to_date) {
-                $query->where('date', '<=', $to_date);
+                $query->where('payment_date', '<=', $to_date);
             }
         }]);
 
