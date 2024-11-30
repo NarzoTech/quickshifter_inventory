09c5971a2c51610ffbc6bf1b368a80a344cec32f
progRizvi <prog.rizvi@gmail.com>
2024-11-30 13:26:22 +0600

    style file seperated

--- main_files/resources/views/admin/layouts/master.blade.php
+++ main_files/resources/views/admin/layouts/master.blade.php
@@ -24,41 +24,9 @@
         href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet">
 
+    @include('admin.layouts.styles')
 
-
-    <link rel="stylesheet" href="{{ asset('backend/css/bootstrap4-toggle.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/css/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/css/tagify.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/css/bootstrap-datepicker.min.css') }}">
-
-
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/fonts/boxicons.css') }}" />
-
-    <link rel="stylesheet" href="{{ asset('global/css/all.min.css') }}">
-
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/core.css') }}"
-        class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/theme-default.css') }}"
-        class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{ asset('backend/assets/css/demo.css') }}" />
-
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-    <link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
-
-    <!-- Page CSS -->
-
-
-    <!-- Helpers -->
-    <script src="{{ asset('backend/assets/vendor/js/helpers.js') }}"></script>
-
-    <script src="{{ asset('backend/assets/vendor/js/template-customizer.js') }}"></script>
-
-    <script src="{{ asset('backend/assets/js/config.js') }}"></script>
-
-    <link rel="stylesheet" href="{{ asset('global/toastr/toastr.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/css/dev.css') }}" />
+    @stack('css')
 
 </head>
 
---
+++ main_files/resources/views/admin/layouts/styles.blade.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="{{ asset('backend/css/bootstrap4-toggle.min.css') }}">
+<link rel="stylesheet" href="{{ asset('backend/css/select2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('backend/css/tagify.css') }}">
+<link rel="stylesheet" href="{{ asset('backend/css/bootstrap-datepicker.min.css') }}">
+
+
+<link rel="stylesheet" href="{{ asset('backend/assets/vendor/fonts/boxicons.css') }}" />
+
+<link rel="stylesheet" href="{{ asset('global/css/all.min.css') }}">
+
+<!-- Core CSS -->
+<link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
+<link rel="stylesheet" href="{{ asset('backend/assets/vendor/css/theme-default.css') }}"
+    class="template-customizer-theme-css" />
+<link rel="stylesheet" href="{{ asset('backend/assets/css/demo.css') }}" />
+
+<!-- Vendors CSS -->
+<link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+<link rel="stylesheet" href="{{ asset('backend/assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+
+<!-- Page CSS -->
+
+
+<!-- Helpers -->
+<script src="{{ asset('backend/assets/vendor/js/helpers.js') }}"></script>
+
+<script src="{{ asset('backend/assets/vendor/js/template-customizer.js') }}"></script>
+
+<script src="{{ asset('backend/assets/js/config.js') }}"></script>
+
+<link rel="stylesheet" href="{{ asset('global/toastr/toastr.min.css') }}">
+<link rel="stylesheet" href="{{ asset('backend/css/dev.css') }}" />
