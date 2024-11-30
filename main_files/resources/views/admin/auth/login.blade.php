1054389e42048932b7c14a921a59383127e70c7a
progRizvi <prog.rizvi@gmail.com>
2024-11-30 13:49:05 +0600

    update login page

--- main_files/resources/views/admin/auth/login.blade.php
+++ main_files/resources/views/admin/auth/login.blade.php
@@ -5,13 +5,13 @@
 @section('content')
     <div class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg p-sm-12 p-6">
         <div class="w-px-400 mx-auto mt-12 pt-5">
-            <h4 class="mb-1">Welcome to sneat! 👋</h4>
+            <h4 class="mb-1">Welcome to {{ $setting->app_name }}! 👋</h4>
             <p class="mb-6">Please sign-in to your account and start the adventure</p>
 
             <form id="formAuthentication" class="mb-6" action="{{ route('admin.store-login') }}" method="post">
                 @csrf
                 <div class="mb-6">
-                    <label for="email" class="form-label">Email</label>
+                    <label for="email" class="form-label">{{ __('Email') }}</label>
                     @if (app()->isLocal() && app()->hasDebugModeEnabled())
                         <input id="email exampleInputEmail" type="email" class="form-control" name="email"
                             tabindex="1" autofocus value="admin@gmail.com">
@@ -21,7 +21,7 @@
                     @endif
                 </div>
                 <div class="mb-6 form-password-toggle">
-                    <label class="form-label" for="password">Password</label>
+                    <label class="form-label" for="password">{{ __('Password') }}</label>
                     <div class="input-group input-group-merge">
                         @if (app()->isLocal() && app()->hasDebugModeEnabled())
                             <input id="password exampleInputPassword" type="password" class="form-control" name="password"
