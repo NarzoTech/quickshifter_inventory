7a739a8d463154c1203c2d14e188bc84d0e278b4
progRizvi <prog.rizvi@gmail.com>
2024-11-30 13:40:22 +0600

    login page implemented

--- main_files/resources/views/admin/auth/app.blade.php
+++ main_files/resources/views/admin/auth/app.blade.php
@@ -1,160 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <link rel="shortcut icon" href="" type="image/x-icon">
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
 
 
-    @yield('title')
-
-    <link rel="icon" href="{{ asset($setting->favicon) }}">
-    <link rel="stylesheet" href="{{ url('backend/assets/css/base.min.css') }}">
-    <script src="{{ asset('global/js/jquery-3.7.1.min.js') }}"></script>
-
-    <style>
-        .app-logo {
-            background-image: url({{ asset($setting->logo) }});
-        }
-    </style>
-
-</head>
-
-<body>
-    <div class="app-container app-theme-white body-tabs-shadow">
-        <div class="app-container">
-            <div class="h-100">
-                @yield('content')
-            </div>
-        </div>
-    </div>
-
-    @include('admin.layouts.javascripts')
-
-    <script>
-        @if (Session::has('messege'))
-            var type = "{{ Session::get('alert-type', 'info') }}"
-            switch (type) {
-                case 'info':
-                    toastr.info("{{ Session::get('messege') }}");
-                    break;
-                case 'success':
-                    toastr.success("{{ Session::get('messege') }}");
-                    break;
-                case 'warning':
-                    toastr.warning("{{ Session::get('messege') }}");
-                    break;
-                case 'error':
-                    toastr.error("{{ Session::get('messege') }}");
-                    break;
-            }
-        @endif
-    </script>
-
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <script>
-                toastr.error('{{ $error }}');
-            </script>
-        @endforeach
-    @endif
-
-</body>
-
-</html>
-
-
-
-
-
-<!DOCTYPE html>
-
-
-<html lang="en" class="light-style layout-wide  customizer-hide" dir="ltr" data-theme="theme-default"
-    data-assets-path="../../assets/" data-template="vertical-menu-template" data-style="light">
+<html lang="en" class="light-style layout-menu-fixed layout-compact layout-navbar-fixed" dir="ltr"
+    data-theme="theme-default" data-style="light" data-assets-path="{{ asset('backend/assets') }}/"
+    data-template="vertical-menu-template">
 
 <head>
     <meta charset="utf-8" />
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Demo : Login Cover - Pages | sneat - Bootstrap Dashboard PRO</title>
+    @yield('title')
+    <link rel="icon" href="{{ asset($setting->favicon) }}">
 
-
-    <meta name="description"
-        content="Most Powerful &amp; Comprehensive Bootstrap 5 Admin Dashboard built for developers!" />
-    <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
-    <!-- Canonical SEO -->
-    <link rel="canonical" href="https://themeselection.com/item/sneat-dashboard-pro-bootstrap/">
-
-
-    <!-- ? PROD Only: Google Tag Manager (Default ThemeSelection: GTM-5DDHKGP, PixInvent: GTM-5J3LMKC) -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-5DDHKGP');
-    </script>
-    <!-- End Google Tag Manager -->
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet">
-
-    <!-- Icons -->
-    <link rel="stylesheet" href="../../assets/vendor/fonts/boxicons.css" />
-    <link rel="stylesheet" href="../../assets/vendor/fonts/fontawesome.css" />
-    <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
-
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="../../assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="../../assets/css/demo.css" />
-
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-    <link rel="stylesheet" href="../../assets/vendor/libs/typeahead-js/typeahead.css" />
-    <!-- Vendor -->
-    <link rel="stylesheet" href="../../assets/vendor/libs/@form-validation/form-validation.css" />
-
-    <!-- Page CSS -->
-    <!-- Page -->
-    <link rel="stylesheet" href="../../assets/vendor/css/pages/page-auth.css">
-
-    <!-- Helpers -->
-    <script src="../../assets/vendor/js/helpers.js"></script>
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-    <script src="../../assets/vendor/js/template-customizer.js"></script>
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="../../assets/js/config.js"></script>
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/page-auth.css') }}">
+    @include('admin.layouts.styles')
 
 </head>
 
 <body>
-
-
-    <!-- ?PROD Only: Google Tag Manager (noscript) (Default ThemeSelection: GTM-5DDHKGP, PixInvent: GTM-5J3LMKC) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DDHKGP" height="0" width="0"
-            style="display: none; visibility: hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
 
     <!-- Content -->
 
@@ -214,125 +78,27 @@
             <!-- /Left Text -->
             <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center p-5">
                 <div class="w-100 d-flex justify-content-center">
-                    <img src="../../assets/img/illustrations/boy-with-rocket-light.png" class="img-fluid"
+                    <img src="{{ asset('backend/assets/img/boy-with-rocket-light.png') }}" class="img-fluid"
                         alt="Login image" width="700" data-app-dark-img="illustrations/boy-with-rocket-dark.png"
                         data-app-light-img="illustrations/boy-with-rocket-light.png">
                 </div>
             </div>
             <!-- /Left Text -->
 
-            <!-- Login -->
-            <div class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg p-sm-12 p-6">
-                <div class="w-px-400 mx-auto mt-12 pt-5">
-                    <h4 class="mb-1">Welcome to sneat! 👋</h4>
-                    <p class="mb-6">Please sign-in to your account and start the adventure</p>
-
-                    <form id="formAuthentication" class="mb-6" action="index.html" method="GET">
-                        <div class="mb-6">
-                            <label for="email" class="form-label">Email or Username</label>
-                            <input type="text" class="form-control" id="email" name="email-username"
-                                placeholder="Enter your email or username" autofocus>
-                        </div>
-                        <div class="mb-6 form-password-toggle">
-                            <label class="form-label" for="password">Password</label>
-                            <div class="input-group input-group-merge">
-                                <input type="password" id="password" class="form-control" name="password"
-                                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                                    aria-describedby="password" />
-                                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-                            </div>
-                        </div>
-                        <div class="my-8">
-                            <div class="d-flex justify-content-between">
-                                <div class="form-check mb-0 ms-2">
-                                    <input class="form-check-input" type="checkbox" id="remember-me">
-                                    <label class="form-check-label" for="remember-me">
-                                        Remember Me
-                                    </label>
-                                </div>
-                                <a href="auth-forgot-password-cover.html">
-                                    <p class="mb-0">Forgot Password?</p>
-                                </a>
-                            </div>
-                        </div>
-                        <button class="btn btn-primary d-grid w-100">
-                            Sign in
-                        </button>
-                    </form>
-
-                    <p class="text-center">
-                        <span>New on our platform?</span>
-                        <a href="auth-register-cover.html">
-                            <span>Create an account</span>
-                        </a>
-                    </p>
-
-                    <div class="divider my-6">
-                        <div class="divider-text">or</div>
-                    </div>
-
-                    <div class="d-flex justify-content-center">
-                        <a href="javascript:;" class="btn btn-sm btn-icon rounded-circle btn-text-facebook me-1_5">
-                            <i class='bx bxl-facebook-circle'></i>
-                        </a>
-
-                        <a href="javascript:;" class="btn btn-sm btn-icon rounded-circle btn-text-twitter me-1_5">
-                            <i class="tf-icons bx bxl-twitter"></i>
-                        </a>
-
-                        <a href="javascript:;" class="btn btn-sm btn-icon rounded-circle btn-text-github me-1_5">
-                            <i class='bx bxl-github'></i>
-                        </a>
-
-                        <a href="javascript:;" class="btn btn-sm btn-icon rounded-circle btn-text-google-plus">
-                            <i class="tf-icons bx bxl-google"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <!-- /Login -->
+            @yield('content')
         </div>
     </div>
 
     <!-- / Content -->
 
 
-    <div class="buy-now">
-        <a href="https://themeselection.com/item/sneat-dashboard-pro-bootstrap/" target="_blank"
-            class="btn btn-danger btn-buy-now">Buy Now</a>
-    </div>
 
-
-
-
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
-
-    <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="../../assets/vendor/libs/popper/popper.js"></script>
-    <script src="../../assets/vendor/js/bootstrap.js"></script>
-    <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
-    <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
-    <script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
-    <script src="../../assets/vendor/js/menu.js"></script>
-
-    <!-- endbuild -->
-
-    <!-- Vendors JS -->
-    <script src="../../assets/vendor/libs/@form-validation/popular.js"></script>
-    <script src="../../assets/vendor/libs/@form-validation/bootstrap5.js"></script>
-    <script src="../../assets/vendor/libs/@form-validation/auto-focus.js"></script>
-
-    <!-- Main JS -->
-    <script src="../../assets/js/main.js"></script>
+    @include('admin.layouts.javascripts')
 
 
     <!-- Page JS -->
-    <script src="../../assets/js/pages-auth.js"></script>
+    <script src="{{ asset('backend/assets/js/pages-auth.js') }}"></script>
 
 </body>
 
 </html>
-
-<!-- beautify ignore:end -->
--- main_files/resources/views/admin/auth/login.blade.php
+++ main_files/resources/views/admin/auth/login.blade.php
@@ -3,105 +3,53 @@
     <title>{{ __('Login') }}</title>
 @endsection
 @section('content')
-    <div class="h-100 no-gutters row">
-        <div class="d-none d-lg-block col-lg-4">
-            <div class="slider-light">
-                <div class="slick-slider">
-                    <div>
-                        <div class="position-relative h-100 d-flex justify-content-center align-items-center bg-plum-plate"
-                            tabindex="-1">
-                            <div class="slide-img-bg"
-                                style="background-image: url('{{ asset('backend/assets/images/originals/city.jpg') }}');">
-                            </div>
-                            <div class="slider-content">
-                                <h3>Perfect Balance</h3>
-                                <p>ArchitectUI is like a dream. Some think it's too good to be true!
-                                    Extensive collection of unified React Boostrap Components and Elements.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="position-relative h-100 d-flex justify-content-center align-items-center bg-premium-dark"
-                            tabindex="-1">
-                            <div class="slide-img-bg"
-                                style="background-image: url({{ asset('backend/assets/images/originals/citynights.jpg') }});">
-                            </div>
-                            <div class="slider-content">
-                                <h3>Scalable, Modular, Consistent</h3>
-                                <p>Easily exclude the components you don't require. Lightweight, consistent
-                                    Bootstrap based styles across all elements and components</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="position-relative h-100 d-flex justify-content-center align-items-center bg-sunny-morning"
-                            tabindex="-1">
-                            <div class="slide-img-bg"
-                                style="background-image: url({{ asset('backend/assets/images/originals/citydark.jpg') }});">
-                            </div>
-                            <div class="slider-content">
-                                <h3>Complex, but lightweight</h3>
-                                <p>We've included a lot of components that cover almost all use cases for
-                                    any type of application.</p>
-                            </div>
-                        </div>
+    <div class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg p-sm-12 p-6">
+        <div class="w-px-400 mx-auto mt-12 pt-5">
+            <h4 class="mb-1">Welcome to sneat! 👋</h4>
+            <p class="mb-6">Please sign-in to your account and start the adventure</p>
+
+            <form id="formAuthentication" class="mb-6" action="{{ route('admin.store-login') }}" method="post">
+                @csrf
+                <div class="mb-6">
+                    <label for="email" class="form-label">Email</label>
+                    @if (app()->isLocal() && app()->hasDebugModeEnabled())
+                        <input id="email exampleInputEmail" type="email" class="form-control" name="email"
+                            tabindex="1" autofocus value="admin@gmail.com">
+                    @else
+                        <input id="email exampleInputEmail" type="email" class="form-control" name="email"
+                            tabindex="1" autofocus value="{{ old('email') }}">
+                    @endif
+                </div>
+                <div class="mb-6 form-password-toggle">
+                    <label class="form-label" for="password">Password</label>
+                    <div class="input-group input-group-merge">
+                        @if (app()->isLocal() && app()->hasDebugModeEnabled())
+                            <input id="password exampleInputPassword" type="password" class="form-control" name="password"
+                                tabindex="2" value="1234">
+                        @else
+                            <input id="password exampleInputPassword" type="password" class="form-control" name="password"
+                                tabindex="2">
+                        @endif
+                        <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="h-100 d-flex bg-white justify-content-center align-items-center col-md-12 col-lg-8">
-            <div class="mx-auto app-login-box col-sm-12 col-md-10 col-lg-9">
-                <div class="app-logo"></div>
-                <h4 class="mb-0">
-                    <span class="d-block">Welcome back,</span>
-                    <span>Please sign in to your account.</span>
-                </h4>
-                <div class="divider row"></div>
-                <div>
-                    <form novalidate="" id="adminLoginForm" action="{{ route('admin.store-login') }}" method="POST">
-                        @csrf
-                        <div class="form-row">
-                            <div class="col-md-6">
-                                <div class="position-relative form-group">
-                                    <label for="email" class="">Email</label>
-                                    @if (app()->isLocal() && app()->hasDebugModeEnabled())
-                                        <input id="email exampleInputEmail" type="email" class="form-control"
-                                            name="email" tabindex="1" autofocus value="admin@gmail.com">
-                                    @else
-                                        <input id="email exampleInputEmail" type="email" class="form-control"
-                                            name="email" tabindex="1" autofocus value="{{ old('email') }}">
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="position-relative form-group">
-                                    <label for="password" class="">Password</label>
-                                    @if (app()->isLocal() && app()->hasDebugModeEnabled())
-                                        <input id="password exampleInputPassword" type="password" class="form-control"
-                                            name="password" tabindex="2" value="1234">
-                                    @else
-                                        <input id="password exampleInputPassword" type="password" class="form-control"
-                                            name="password" tabindex="2">
-                                    @endif
-                                </div>
-                            </div>
+                <div class="my-8">
+                    <div class="d-flex justify-content-between">
+                        <div class="form-check mb-0 ms-2">
+                            <input class="form-check-input" type="checkbox" id="remember-me">
+                            <label class="form-check-label" for="remember-me">
+                                Remember Me
+                            </label>
                         </div>
-                        <div class="position-relative form-check"><input name="remember" id="exampleCheck" type="checkbox"
-                                class="form-check-input" {{ old('remember') ? 'checked' : '' }}><label for="exampleCheck"
-                                class="form-check-label">Keep me logged in</label></div>
-                        <div class="divider row"></div>
-                        <div class="d-flex align-items-center">
-                            <div class="ml-auto"><a href="{{ route('admin.password.request') }}"
-                                    class="btn-lg btn btn-link">Recover
-                                    Password</a>
-                                <button class="btn btn-primary btn-lg" id="adminLoginBtn" type="submit">Login to
-                                    Dashboard</button>
-                            </div>
-                        </div>
-                    </form>
+                        <a href="auth-forgot-password-cover.html">
+                            <p class="mb-0">Forgot Password?</p>
+                        </a>
+                    </div>
                 </div>
-            </div>
+                <button class="btn btn-primary d-grid w-100" type="submit">
+                    Sign in
+                </button>
+            </form>
         </div>
     </div>
 @endsection
