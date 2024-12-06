c30ccf6a83957678f79d570050f64fe063b62c97
progRizvi <prog.rizvi@gmail.com>
2024-12-06 18:25:08 +0600

    asset list modified

--- main_files/resources/views/admin/pages/asset/type.blade.php
+++ main_files/resources/views/admin/pages/asset/type.blade.php
@@ -1,127 +1,139 @@
 @extends('admin.layouts.master')
 @section('title')
-    <title>{{ __('Asset Type') }}</title>
+    <title>{{ __('Asset Type List') }}</title>
 @endsection
 
 
 @section('content')
-    <div class="main-content">
-        <section class="section">
-
-
-            <div class="section-body">
-                <div class="row">
-                    {{-- Search filter --}}
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <form action="{{ route('admin.asset-category.index') }}" method="GET" class="card-body">
-                                    <div class="row">
-                                        <div class="col-md-4 form-group search-wrapper">
-                                            <input type="text" name="keyword" value="{{ request()->get('keyword') }}"
-                                                class="form-control" placeholder="{{ __('Search') }}">
-                                            <button type="submit">
-                                                <i class="far fa-arrow-alt-circle-right"></i>
-                                            </button>
-                                        </div>
-                                        <div class="col-md-2 form-group">
-                                            <select name="order_by" id="order_by" class="form-control">
-                                                <option value="">{{ __('Order By') }}</option>
-                                                <option value="1" {{ request('order_by') == '1' ? 'selected' : '' }}>
-                                                    {{ __('ASC') }}
-                                                </option>
-                                                <option value="0" {{ request('order_by') == '0' ? 'selected' : '' }}>
-                                                    {{ __('DESC') }}
-                                                </option>
-                                            </select>
-                                        </div>
-                                        <div class="col-md-2 form-group">
-                                            <select name="par-page" id="par-page" class="form-control">
-                                                <option value="">{{ __('Per Page') }}</option>
-                                                <option value="10" {{ '10' == request('par-page') ? 'selected' : '' }}>
-                                                    {{ __('10') }}
-                                                </option>
-                                                <option value="50" {{ '50' == request('par-page') ? 'selected' : '' }}>
-                                                    {{ __('50') }}
-                                                </option>
-                                                <option value="100"
-                                                    {{ '100' == request('par-page') ? 'selected' : '' }}>
-                                                    {{ __('100') }}
-                                                </option>
-                                                <option value="all"
-                                                    {{ 'all' == request('par-page') ? 'selected' : '' }}>
-                                                    {{ __('All') }}
-                                                </option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4>
-                                    <a href="javascript:;" data-bs-toggle="modal" data-bs-target="#addAssetType"
-                                        class="btn btn-primary"><i class="fa fa-plus"></i>
-                                        {{ __('Add Asset Type') }}</a>
-                                </h4>
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive table-invoice">
-                                    <table class="table table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th>{{ __('SN') }}</th>
-                                                <th>{{ __('Name') }}</th>
-                                                <th>{{ __('Action') }}</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @forelse ($types as $index => $type)
-                                                <tr>
-                                                    <td>{{ $loop->first + $index }}</td>
-                                                    <td>{{ $type->name }}</td>
-                                                    <td>
-                                                        <div class="btn-group" role="group">
-                                                            <button id="btnGroupDrop{{ $type->id }}" type="button"
-                                                                class="btn btn-primary dropdown-toggle"
-                                                                data-bs-toggle="dropdown" aria-haspopup="true"
-                                                                aria-expanded="false">
-                                                                Action
-                                                            </button>
-                                                            <div class="dropdown-menu"
-                                                                aria-labelledby="btnGroupDrop{{ $type->id }}">
-                                                                <a class="dropdown-item" href="javascript:;"
-                                                                    data-bs-toggle="modal"
-                                                                    data-bs-target="#editType{{ $type->id }}">Edit</a>
-                                                                <a href="javascript:;" class="dropdown-item"
-                                                                    onclick="deleteData({{ $type->id }})">
-                                                                    Delete</a>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            @empty
-                                                <x-empty-table :name="__('Bank')" route="" create="no"
-                                                    :message="__('No data found!')" colspan="6"></x-empty-table>
-                                            @endforelse
-                                        </tbody>
-                                    </table>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body pb-1">
+                    <form class="search_form" action="" method="GET">
+                        <div class="row">
+                            <div class="col-xxl-3 col-md-4">
+                                <div class="form-group search-wrapper">
+                                    <input type="text" name="keyword" value="{{ request()->get('keyword') }}"
+                                        class="form-control" placeholder="Search..." autocomplete="off">
+                                    <button type="submit">
+                                        <i class='bx bx-search'></i>
+                                    </button>
                                 </div>
-                                @if (request()->get('par-page') !== 'all')
-                                    <div class="float-right">
-                                        {{ $types->onEachSide(0)->links() }}
-                                    </div>
-                                @endif
+                            </div>
+                            <div class="col-xxl-2 col-md-4">
+                                <div class="form-group">
+                                    <select name="order_type" id="order_type" class="form-control">
+                                        <option value="id" {{ request('order_type') == 'id' ? 'selected' : '' }}>
+                                            {{ __('Serial') }}</option>
+                                        <option value="name" {{ request('order_type') == 'name' ? 'selected' : '' }}>
+                                            {{ __('Name') }}</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-xxl-2 col-md-4">
+                                <div class="form-group">
+                                    <select name="order_by" id="order_by" class="form-control">
+                                        <option value="">{{ __('Order By') }}</option>
+                                        <option value="asc" {{ request('order_by') == 'asc' ? 'selected' : '' }}>
+                                            {{ __('ASC') }}
+                                        </option>
+                                        <option value="desc" {{ request('order_by') == 'desc' ? 'selected' : '' }}>
+                                            {{ __('DESC') }}
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-xxl-2 col-md-4">
+                                <div class="form-group">
+                                    <select name="par-page" id="par-page" class="form-control">
+                                        <option value="">{{ __('Per Page') }}</option>
+                                        <option value="10" {{ '10' == request('par-page') ? 'selected' : '' }}>
+                                            {{ __('10') }}
+                                        </option>
+                                        <option value="50" {{ '50' == request('par-page') ? 'selected' : '' }}>
+                                            {{ __('50') }}
+                                        </option>
+                                        <option value="100" {{ '100' == request('par-page') ? 'selected' : '' }}>
+                                            {{ __('100') }}
+                                        </option>
+                                        <option value="all" {{ 'all' == request('par-page') ? 'selected' : '' }}>
+                                            {{ __('All') }}
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-xxl-1 col-md-4">
+                                <div class="form-group">
+                                    <button type="submit" class="btn bg-label-danger form-reset"><i
+                                            class='bx bx-rotate-right'></i></button>
+
+                                    <button type="submit" class="btn bg-label-primary"><i
+                                            class='bx bx-search'></i></button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
+
+    <div class="card mt-3 mb-3">
+        <div class="card-header">
+            <div class="card-header-title font-size-lg text-capitalize font-weight-normal">
+                <h4 class="section_title"><i class="fas fa-list"></i> {{ __('Asset Type List') }}</h4>
+            </div>
+            <div class="btn-actions-pane-right actions-icon-btn">
+                <a href="javascript:;" data-bs-toggle="modal" data-bs-target="#addAssetType" class="btn btn-primary"><i
+                        class="fa fa-plus"></i>
+                    {{ __('Add Asset Type') }}</a>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive list_table">
+                <table style="width: 100%;" class="table mb-3">
+                    <thead>
+                        <tr>
+                            <th>{{ __('SN') }}</th>
+                            <th>{{ __('Name') }}</th>
+                            <th>{{ __('Action') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($types as $index => $type)
+                            <tr>
+                                <td>{{ $loop->first + $index }}</td>
+                                <td>{{ $type->name }}</td>
+                                <td>
+                                    <div class="btn-group" role="group">
+                                        <button id="btnGroupDrop{{ $type->id }}" type="button"
+                                            class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown"
+                                            aria-haspopup="true" aria-expanded="false">
+                                            Action
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="btnGroupDrop{{ $type->id }}">
+                                            <a class="dropdown-item" href="javascript:;" data-bs-toggle="modal"
+                                                data-bs-target="#editType{{ $type->id }}">Edit</a>
+                                            <a href="javascript:;" class="dropdown-item"
+                                                onclick="deleteData({{ $type->id }})">
+                                                Delete</a>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @empty
+                            <x-empty-table :name="__('Bank')" route="" create="no" :message="__('No data found!')"
+                                colspan="3"></x-empty-table>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+            @if (request()->get('par-page') !== 'all')
+                <div class="float-right">
+                    {{ $types->onEachSide(0)->links() }}
+                </div>
+            @endif
+        </div>
     </div>
 
     {{-- add Asset Type --}}
