daaa900aee4c12d439a316f58083c149b22ea6ed
sazalahmed <sazal.web@gmail.com>
2024-12-16 23:15:01 +0600

    product section updated

--- main_files/Modules/Product/resources/views/products/single-product-modal.blade.php
+++ main_files/Modules/Product/resources/views/products/single-product-modal.blade.php
@@ -1,38 +1,35 @@
+<div class="modal-header mb-0">
+    <h4 class="modal-title">View Product</h4>
+    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+</div>
 <div class="modal-body">
     <div class="row">
         <div class="col-sm-9 col-md-9">
             <div class="row">
                 <div class="col-sm-6 invoice-col">
-                    <b>Product Name: </b> {{ $product->name }}<br>
-                    <b>Barcode:</b>{{ $product->barcode }}<br>
-                    <b>Brand: </b>{{ $product->brand->name }}<br>
-                    <b>Unit: </b>{{ $product->unit->name }}<br>
-                    <span style="display: none;">
-                        <strong>Available in locations:</strong>
-                        Quick Shifter,
-                    </span>
-                    <br>
-                    <b>Created At:
-                    </b>{{ $product->created_at->format('d F, Y') }}
+                    <p class="mb-2"><b>Product Name: </b> {{ $product->name }}</p>
+                    <p class="mb-2"><b>Barcode:</b>{{ $product->barcode }}</p>
+                    <p class="mb-2"><b>Brand: </b>{{ $product->brand->name }}</p>
+                    <p class="mb-2"><b>Unit: </b>{{ $product->unit->name }}</p>
+                    <p class="mb-2"> <b class="d-none">Available in locations: </b> Quick Shifter</p>
+                    <p class="mb-2"><b>Created At: </b>{{ $product->created_at->format('d F, Y') }}</p>
                 </div>
 
                 <div class="col-sm-6 invoice-col">
-                    <b>Category:
-                    </b>{{ $product->category->name }}<br>
-                    <b>Manage Stock: </b>{{ $product->stock_alert ? 'Yes' : 'No' }}<br>
-                    <b>Alert quantity: </b>{{ $product->stock_alert }} <br>
-                    <b>Has IMEI/Model No: </b>{{ $product->hasVariant ? 'Yes' : 'No' }} <br>
-                    <b>Updated At: </b>{{ $product->updated_at->format('d F, Y') }}
+                    <p class="mb-2"><b>Category: </b>{{ $product->category->name }}</p>
+                    <p class="mb-2"><b>Manage Stock: </b>{{ $product->stock_alert ? 'Yes' : 'No' }}</p>
+                    <p class="mb-2"><b>Alert quantity: </b>{{ $product->stock_alert }} </p>
+                    <p class="mb-2"><b>Has IMEI/Model No: </b>{{ $product->hasVariant ? 'Yes' : 'No' }} </p>
+                    <p class="mb-2"><b>Updated At: </b>{{ $product->updated_at->format('d F, Y') }}</p>
                 </div>
                 <div class="clearfix"></div>
                 <br>
-                <div class="col-sm-12">
-                </div>
+                <div class="col-sm-12"> </div>
             </div>
         </div>
 
         <div class="col-sm-3 col-md-3">
-            <div class="thumbnail">
+            <div class="thumbnail view_product">
                 <img src="{{ $product->singleImage }}" class="img-fluid" alt="Product image">
             </div>
         </div>
@@ -47,12 +44,12 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="table-responsive">
-                        <table class="table table-condensed bg-gray">
+                        <table class="table mb-0 table-condensed">
                             <thead>
-                                <tr class="bg-success">
-                                    <th>Barcode</th>
-                                    <th>Unit price</th>
-                                    <th>stock</th>
+                                <tr>
+                                    <th><b>Barcode</b></th>
+                                    <th><b>Unit price</b></th>
+                                    <th><b>stock</b></th>
                                 </tr>
                             </thead>
                             <tbody>
