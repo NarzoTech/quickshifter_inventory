b187747fc2488df40429e623287f939954510d1d
sazalahmed <sazal.web@gmail.com>
2024-12-29 01:54:50 +0600

    footer design updated

--- Modules/Supplier/resources/views/due-pay-history.blade.php
+++ Modules/Supplier/resources/views/due-pay-history.blade.php
@@ -75,7 +75,7 @@
             </div>
         </div>
     </div>
-    <div class="card mt-5 mb-5">
+    <div class="card mt-5">
         <div class="card-header-tab card-header">
             <div class="card-header-title font-size-lg text-capitalize font-weight-normal">
                 <h4 class="section_title"> {{ __('Supplier Due Pay List') }}</h4>
